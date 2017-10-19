e6c26ca0306aec1d3c7a3a2f2bf3f5fb7592db50
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-10-18 20:58:02 -0500

    data for local forcast printing to screen

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,36 +1,45 @@
 "use strict";
-const domString = (movieArray, imgConfig) => {
+
+// Temperature
+// Conditions
+// Air pressure
+// Wind speed
+// An affordance to view the forecast for the current day, the next three days, or the next 7 days
+
+const domString = (weatherArray) => {
 	let domString = '';
-	for(let i =0; i < movieArray.length; i++) {
+	for(let i =0; i < weatherArray.length; i++) {
 		if (i % 3 === 0){
 			domString += `<div class ="row">`;
 		}
 		
 	  	domString += `<div class="col-sm-6 col-md-4">`;
 	    domString += 	`<div class="thumbnail">`;
-	    domString +=	  `<img src="${imgConfig.base_url}/w342/${movieArray[i].poster_path}"
- alt="">`;
+	//     domString +=	  `<img src="${imgConfig.base_url}/w342/${movieArray[i].poster_path}"
+ // alt="">`;
 	    domString +=  `<div class="caption">`;
-	    domString +=    `<h3>${movieArray[i].original_title}</h3>`;
-	    domString +=    `<p>${movieArray[i].overview}</p>`;
-	    domString +=    `<p><a href="#" class="btn btn-primary" role="button">Review</a> <a href="#" class="btn btn-default" role="button">Watchlist</a></p>`;
+	    domString +=    `<h3>${weatherArray[i].name}</h3>`;
+	    domString +=    `<p>${weatherArray[i].temp}</p>`;
+	   	domString +=    `<p>${weatherArray[i].conditions}</p>`;
+	    domString +=    `<p>${weatherArray[i].pressure}</p>`;
+	    domString +=    `<p>${weatherArray[i].wind}</p>`;
+
+	    domString +=    `<p><a href="#" class="btn btn-primary" role="button">3 day forecast</a> <a href="#" class="btn btn-default" role="button">5 day forecast</a></p>`;
 	    domString +=  		`</div>`;
-	    domString += 	`</div>`;
 	  	domString +=  `</div>`;
-	  	if (i % 3 === 2 || i === movieArray.length -1) {
 		domString += `</div>`;
-		}
+		
 	}
 
 		printToDom(domString);
 };
 
 const printToDom = (strang) => {
-	$("#movies").append(strang);
+	$("#localWeather").append(strang);
 };
 
 const clearDom = () => {
-	$('#movies').empty();
+	$('#localWeather').empty();
 };
 
 
